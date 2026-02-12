53a064c6e1ccba3b1e4a11257c58427b62424a9d
Chandl3r <38330610+Chandl3r@users.noreply.github.com>
2026-02-12 16:59:15 +0100

    Add files via upload
    Sostituito file server.js

--- server.js
+++ server.js
@@ -6,12 +6,14 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// CORS per permettere chiamate da Altervista
+// CORS per permettere chiamate da browser (da qualsiasi dominio)
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-    if (req.method === 'OPTIONS') return res.sendStatus(200);
+    if (req.method === 'OPTIONS') {
+        return res.status(200).end();
+    }
     next();
 });
 
